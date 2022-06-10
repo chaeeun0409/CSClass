2ffffcbf8540ec0350ee3d89cdeaafbd8e2927f6
chaeeun0409 <s2017@e-mirim.hs.kr>
2022-06-10 11:04:12 +0900

    23-2. 상속

--- CSClass/Dog.cs
+++ CSClass/Dog.cs
@@ -2,31 +2,18 @@
 
 namespace CSClass
 {
-    public class Dog
+    public class Dog :Animal
        
     {
-        public int Age { get; set; }
         public String Color  { get; set; }
-
-        public Dog()
-        {
-            this.Age = 0;
-        }
         public void Eat()
         {
-            Console.WriteLine("냠냠 먹습니다");
+            Console.WriteLine("쩝쩝 먹겠습니다");
         }
-
-        public void Sleep()
-        {
-            Console.WriteLine("쿨쿨 잠을 잡니다");
-
-        }
-
         public void Bark()
         {
-            Console.WriteLine("왈왈 짖습니다");
-
+            base.Eat();
+            Console.WriteLine("멍멍 짖습니다");
         }
     }
 }
